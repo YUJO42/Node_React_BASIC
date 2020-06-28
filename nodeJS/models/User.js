52903ac23b42e130d328843954cba43d7437951f
yujo <yujo@student.42seoul.kr>
2020-06-28 16:35:08 +0900

    Mongoose Schema exports Error pix

--- nodeJS/models/User.js
+++ nodeJS/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = mongoose.Schema({
+const userSchema = new Schema({
   name: {
     type: String,
     maxlength: 50,
@@ -31,6 +32,6 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
-module.exports { User }
+module.exports = mongoose.model("User", userSchema);
