4c7fb1ddc019eb94e7949c9f07eb683cb2e8e794
yujo <yujo@student.42seoul.kr>
2021-01-02 21:15:50 +0900

    feat: 회원가입 기능

--- boilar-plate/index.js
+++ boilar-plate/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-const MONGO_DB_KEY = require('./key/mongo.js');
-console.log(MONGO_DB_KEY);
-
+const MONGO_DB_KEY = require('./key/mongo');
 mongoose
   .connect(
     `mongodb+srv://yujo:${MONGO_DB_KEY}@boiler-plate.c5ep7.mongodb.net/<dbname>?retryWrites=true&w=majority`,
@@ -20,5 +23,19 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/register', (req, res) => {
+  // 회원 가입시 필요한 정보를 client에서 가져오면 그것들을 DB에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(PORT, () => console.log(`localhost:${PORT} now running🏃‍`));
