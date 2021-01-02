da64e67b715cded3a2f86785405d39020ebaaa83
yujo <yujo@student.42seoul.kr>
2021-01-02 20:19:39 +0900

    feat: connect MongoDB

--- boilar-plate/index.js
+++ boilar-plate/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://yujo:${password}@boiler-plate.c5ep7.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    },
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`localhost:${port} now running🏃‍♂️`));
+app.listen(port, () => console.log(`localhost:${port} now running🏃‍`));
