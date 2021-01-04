77ef280be22906025a1a74c32bf9c490358a6681
yujo <yujo@student.42seoul.kr>
2021-01-04 20:48:52 +0900

    chore: git set

--- movie-app/client/src/components/Config.js
+++ movie-app/client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '3a62660e6e7ba6902347c7ab5b3dd0b1';
+export const IMG_BASE_URL = 'https://image.tmdb.org/t/p/';
