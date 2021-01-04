32aa9afe7ed39a57b63148d294a6cbfdba01c9a9
yujo <yujo@student.42seoul.kr>
2021-01-04 14:53:38 +0900

    feat: boiler-plate init

--- movie-app/client/src/hoc/auth.js
+++ movie-app/client/src/hoc/auth.js
@@ -1,32 +1,44 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { auth } from '../_actions/user_action';
+import { auth } from '../_actions/user_actions';
+import { useSelector, useDispatch } from "react-redux";
 
-export default function Auth(SpecificComponent, option, adminRoute = null) {
-  function AuthenticationCheck(props) {
-    const dispatch = useDispatch();
+export default function (SpecificComponent, option, adminRoute = null) {
+    function AuthenticationCheck(props) {
 
-    useEffect(() => {
-      dispatch(auth()).then((response) => {
-        console.log(response);
+        let user = useSelector(state => state.user);
+        const dispatch = useDispatch();
 
-        if (!response.payload.isAuth) {
-          if (option) {
-            props.history.push('/login');
-          }
-        } else {
-          if (adminRoute && !response.payload.isAdmin) {
-            props.history.push('/');
-          } else {
-            if (option === false) {
-              props.history.push('/');
-            }
-          }
-        }
-      });
-    });
-    return <SpecificComponent />;
-  }
+        useEffect(() => {
+            //To know my current status, send Auth request 
+            dispatch(auth()).then(response => {
+                //Not Loggined in Status 
+                if (!response.payload.isAuth) {
+                    if (option) {
+                        props.history.push('/login')
+                    }
+                    //Loggined in Status 
+                } else {
+                    //supposed to be Admin page, but not admin person wants to go inside
+                    if (adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/')
+                    }
+                    //Logged in Status, but Try to go into log in page 
+                    else {
+                        if (option === false) {
+                            props.history.push('/')
+                        }
+                    }
+                }
+            })
 
-  return AuthenticationCheck;
+        }, [])
+
+        return (
+            <SpecificComponent {...props} user={user} />
+        )
+    }
+    return AuthenticationCheck
 }
+
+
