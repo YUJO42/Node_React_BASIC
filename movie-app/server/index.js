d77be14bd856d094d66bd7a012c6fbbf78db1bb8
yujo <yujo@student.42seoul.kr>
2021-01-04 14:19:29 +0900

    feat: boiler-plate init

---
+++ movie-app/server/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/api/hello', (req, res) => {
+  res.send('안녕하세여');
+});
+
+app.post('/api/users/register', (req, res) => {
+  // 회원 가입시 필요한 정보를 client에서 가져오면 그것들을 DB에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post('/api/users/login', (req, res) => {
+  // 요청된 이메일이 DB에 존재하는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '제공된 이메일에 해당되는 유저가 없습니다.',
+      });
+    }
+
+    // 비밀번호가 일치하는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다.',
+        });
+      }
+
+      // 비밀번호가 맞다면 토큰을 생성
+      user.generateToken((err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+
+        // 토큰을 쿠키에 저장한다
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    role: req.user.role,
+  });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
+
+app.listen(PORT, () => console.log(`localhost:${PORT} now running🏃‍`));
