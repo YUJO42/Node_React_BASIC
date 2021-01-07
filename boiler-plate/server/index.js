321437a9541dd9d6a3b794f0f4430083572a62fb
yujo <yujo@student.42seoul.kr>
2021-01-07 13:31:46 +0900

    refactor: boiler-plate reset & init

--- boiler-plate/server/index.js
+++ boiler-plate/server/index.js
@@ -1,105 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 5000;
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const path = require("path");
+const cors = require('cors')
 
-const config = require('./config/key');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const { auth } = require('./middleware/auth');
-const { User } = require('./models/User');
+const config = require("./config/key");
 
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+app.use(cors())
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const mongoose = require('mongoose');
+app.use('/api/users', require('./routes/users'));
 
-mongoose
-  .connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
 
-app.get('/api/hello', (req, res) => {
-  res.send('안녕하세여');
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
 
-app.post('/api/users/register', (req, res) => {
-  // 회원 가입시 필요한 정보를 client에서 가져오면 그것들을 DB에 넣어준다.
-  const user = new User(req.body);
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
 
-  user.save((err, doc) => {
-    if (err) {
-      return res.json({ success: false, err });
-    }
-    return res.status(200).json({
-      success: true,
-    });
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
+}
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`)
 });
-
-app.post('/api/users/login', (req, res) => {
-  // 요청된 이메일이 DB에 존재하는지 확인
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: '제공된 이메일에 해당되는 유저가 없습니다.',
-      });
-    }
-
-    // 비밀번호가 일치하는지 확인
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch) {
-        return res.json({
-          loginSuccess: false,
-          message: '비밀번호가 틀렸습니다.',
-        });
-      }
-
-      // 비밀번호가 맞다면 토큰을 생성
-      user.generateToken((err, user) => {
-        if (err) {
-          return res.status(400).send(err);
-        }
-
-        // 토큰을 쿠키에 저장한다
-        res
-          .cookie('x_auth', user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id });
-      });
-    });
-  });
-});
-
-app.get('/api/users/auth', auth, (req, res) => {
-  res.status(200).json({
-    _id: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-    role: req.user.role,
-  });
-});
-
-app.get('/api/users/logout', auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
-    if (err) {
-      return res.json({ success: false, err });
-    }
-    return res.status(200).send({
-      success: true,
-    });
-  });
-});
-
-app.listen(PORT, () => console.log(`localhost:${PORT} now running🏃‍`));
--- boiler-plate/server/middleware/auth.js
+++ boiler-plate/server/middleware/auth.js
@@ -1,17 +1,15 @@
 const { User } = require('../models/User');
 
-const auth = (req, res, next) => {
-  // 클라이언트 쿠키에서 토큰을 가져온다
-  const token = req.cookies.x_auth;
+let auth = (req, res, next) => {
+  let token = req.cookies.w_auth;
 
-  // 토큰을 복호화 한 후 유저를 찾는다.
   User.findByToken(token, (err, user) => {
-    if (err) {
-      throw err;
-    }
-    if (!user) {
-      return res.json({ isAuth: false, error: true });
-    }
+    if (err) throw err;
+    if (!user)
+      return res.json({
+        isAuth: false,
+        error: true
+      });
 
     req.token = token;
     req.user = user;
